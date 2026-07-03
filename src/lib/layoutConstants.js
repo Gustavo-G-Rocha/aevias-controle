9e4259d120ab73dd997a241dda4a43e8975ea741
gustavo.rocha <gustavo.rocha@afirmaevias.com.br>
2026-07-03 12:42:04 +0000

    File changes

--- src/lib/layoutConstants.js
+++ src/lib/layoutConstants.js
@@ -49,7 +49,6 @@ export const REPORT_PAGES = new Set([
   "RelatorioGranuMistura",
   "RelatorioUnificado",
   "RelatorioCertificacaoUsina",
-  "ImpressionEtiquetas",
 ]);
 
 /** Returns the tab zone key for a given pathname, or null */
